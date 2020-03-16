16d8ff970b3941f2dbf43df4ed565a2468c381bd
daafith <davidbaak@gmail.com>
2020-03-16 17:18:23 +0100

    dependency update

--- populate.sh
+++ populate.sh
@@ -1,12 +1,15 @@
+echo "------------------"
+echo "inserting products"
+echo "------------------"
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"identifier":"PR101","name":"Pringles","price":149}' \
   http://localhost:1234/product
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"identifier":"FI101","name":"Fire balls","price":89}' \
+  --data '{"identifier":"FI101","name":"Fire Balls","price":89}' \
   http://localhost:1234/product
-  curl --header "Content-Type: application/json" \
+curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"identifier":"CC101","name":"Chocolate Chip Cookies","price":299}' \
   http://localhost:1234/product
