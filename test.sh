fd6b7f508459aec1cb3f580189bbbf72a0ab9c7c
daafith <davidbaak@gmail.com>
2020-03-19 14:58:01 +0100

    testing with bash

--- test.sh
+++ test.sh
@@ -25,7 +25,7 @@ validate() {
     echo "$2" TEST OK
     addToOK
     else
-    echo "$2" TEST FAIL, result is 
+    echo "$2" TEST FAIL, the actual result is 
     echo "$1"
     addToFail
   fi
@@ -41,6 +41,26 @@ failPostConflict=$(curl --header "Content-Type: application/json" \
   --data '{"name":"Sponges","unitPrice":99,"unitSize":8,"unitType":"ITEM"}' \
   http://localhost:1234/product)
 
+failPostWrongPrice=$(curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"Foobar","unitPrice":4,"unitSize":8,"unitType":"ITEM"}' \
+  http://localhost:1234/product)
+
+failPostWrongSize=$(curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"Foobar","unitPrice":55,"unitSize":0,"unitType":"ITEM"}' \
+  http://localhost:1234/product)
+
+failPostWrongItemType=$(curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"Foobar","unitPrice":55,"unitSize":23,"unitType":"BOX"}' \
+  http://localhost:1234/product)
+
+failPostMissingParam=$(curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"Foobar","unitPrice":55,"unitSize":30}' \
+  http://localhost:1234/product)
+
 echo
 echo "------------------"
 echo "Testing GET"
@@ -48,11 +68,17 @@ echo "------------------"
 echo 
 
 succesGet=$(curl --request GET http://localhost:1234/product/Sponges)
+succesGetAll=$(curl --request GET http://localhost:1234/product)
 
 echo 
 validate "$succesPost" "Product registered"
 validate "$failPostConflict" "Product already exists"
+validate "$failPostWrongPrice" "Price must be 5 or higher"
+validate "$failPostWrongSize" "Size must be 1 or higher"
+validate "$failPostWrongItemType" "\"unitType\": \"BOX is not a valid choice"
+validate "$failPostMissingParam" "\"unitType\": \"Missing required parameter"
 validate "$succesGet" "Product found"
+validate "$succesGetAll" "1 Product(s) found"
 
 echo 
 echo Test Summary:
