f77c5abe36fd8550152a07c8d236e5024c56c18d
daafith <davidbaak@gmail.com>
2020-03-19 15:28:17 +0100

    test script finished

--- test.sh
+++ test.sh
@@ -63,12 +63,38 @@ failPostMissingParam=$(curl --header "Content-Type: application/json" \
 
 echo
 echo "------------------"
+echo "Testing PUT"
+echo "------------------"
+echo 
+
+succesPut=$(curl --header "Content-Type: application/json" \
+  --request PUT \
+  --data '{"name":"Sponges","unitPrice":199,"unitSize":8,"unitType":"ITEM"}' \
+  http://localhost:1234/product)
+
+failPutNotFound=$(curl --header "Content-Type: application/json" \
+  --request PUT \
+  --data '{"name":"Spongebob","unitPrice":199,"unitSize":8,"unitType":"ITEM"}' \
+  http://localhost:1234/product)
+
+echo
+echo "------------------"
 echo "Testing GET"
 echo "------------------"
 echo 
 
 succesGet=$(curl --request GET http://localhost:1234/product/Sponges)
 succesGetAll=$(curl --request GET http://localhost:1234/product)
+failGet=$(curl --request GET http://localhost:1234/product/idontexist)
+
+echo
+echo "------------------"
+echo "Testing DELETE"
+echo "------------------"
+echo 
+
+succesDelete=$(curl --request DELETE http://localhost:1234/product/Sponges)
+failDelete=$(curl --request DELETE http://localhost:1234/product/idontexist)
 
 echo 
 validate "$succesPost" "Product registered"
@@ -77,8 +103,13 @@ validate "$failPostWrongPrice" "Price must be 5 or higher"
 validate "$failPostWrongSize" "Size must be 1 or higher"
 validate "$failPostWrongItemType" "\"unitType\": \"BOX is not a valid choice"
 validate "$failPostMissingParam" "\"unitType\": \"Missing required parameter"
+validate "$succesPut" "Product updated"
+validate "$failPutNotFound" "Product not found"
 validate "$succesGet" "Product found"
 validate "$succesGetAll" "1 Product(s) found"
+validate "$failGet" "Product not found"
+validate "$succesDelete" "Product removed"
+validate "$failDelete" "Product not found"
 
 echo 
 echo Test Summary:
